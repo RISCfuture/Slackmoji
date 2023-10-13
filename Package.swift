d0106945d06b0d10ce409314996d2b523153da5e
Tim Morgan <github@timothymorgan.info>
2023-10-13 12:25:02 -0500

    Swift 5.9; emoji updates

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Slackmoji",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v14)],
     products: [
         .library(
             name: "Slackmoji",
